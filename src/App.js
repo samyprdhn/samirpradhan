b4aba63637815df171f9939b8882aad2566c1eb6
samyprdhn <samyprdhn@gmail.com>
2023-03-21 01:37:59 -0400

    feat: updates to the profile

--- src/App.js
+++ src/App.js
@@ -14,10 +14,29 @@ function App() {
         <p>
           <code>  💎 🙌  </code>
             <code> </code>
-          <code> Samir Pradhan</code>
-          <code>Engineering | Finance </code>
+            <code><strong> Samir Pradhan</strong></code>
+          <code><strong>Engineering | Finance </strong></code>
+
+            <p><strong>
+                <br></br> Computer professional with 8 to 9 years of experience in the tech industry with a passion for finance and the global markets. <br></br>
+
+                <br></br>Excellent technical skills in developing enterprise software and designing cloud architectures. Having worked with various programming languages such as Java, Kotlin, Python, Javascript, I am well-equipped to handle complex projects.<br></br>
+
+                <br></br>Hands-on experience working with databases such as MySQL, Oracle, DynamoDB, PostgreSQL, Cassandra, and MongoDB, as well as various technologies including but not limited to JDBC, Hibernate, GraphQL, Elasticsearch, JQuery, AJAX, Angular/React, MVC, Spring framework, Conda/Anaconda, Ansible, Splunk, ELK, Jenkins (CI), git/GitHub/GitLab, REST/SOAP, Docker, Maven and Gradle build tools, HTML, CSS, JSON/XML/YAML etc ...<br></br>
+
+                <br></br>Cloud practitioner -> AWS (certified), GCP and Azure.<br></br>
+
+                <br></br>Well versed with tools like Jira, Confluence and Asana to participate in Agile methodologies. Certified Scrum Product Owner (CSPO).<br></br>
+
+                <br></br>Proficient with MS Office, Google G and Adobe suites.<br></br>
+
+                <br></br>Business/Finance oriented, banking, entrepreneurship, import/export, investing, stock, options and forex trading, cryptocurrencies, portfolio/asset management, technical and financial analysis, income statements/balance sheets, accounting, Blockchain evangelist, Ethereum, Bitcoin, Solidity, DeFi, NFTs, DApps.<br></br>
+            </strong>
+            </p>
+
         </p>
       </header>
+
 
       <footer className="App-footer">
           <a href={"https://www.linkedin.com/in/-samir-pradhan-/"}><img src={linkedin}/></a>
